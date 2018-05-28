69a8d4ed16f1538dc3c751f467ca0418be1aacda
lhuynh27 <leannehuynh27@gmail.com>
2018-05-28 15:53:49 -0700

    Passed all test for num, str, null, und, boolean,arr, obj,func

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,40 @@
 
 // but you don't so you're going to write it from scratch:
 
+//Things to checK:
+//Number
+//String
+//Null
+//Undefined
+//Boolean
+//Array
+//Obj
+//Function
+
 var stringifyJSON = function(obj) {
-  // your code goes here
+	var outcome = [];
+	if (typeof obj === 'number' || typeof obj === 'boolean'){
+		return obj.toString();
+	}
+	if (typeof obj === 'string'){
+		return '"' + obj + '"';
+	}
+	if (obj === null){
+		return "null";
+	} 
+	if (obj === undefined){
+		return "undefined";
+	}
+	if (Array.isArray(obj)){
+		obj = obj.map(function(input){ return stringifyJSON(input);})
+		//change each input to string (multiple individual strings)
+		return "[" + obj.join() + "]"; 
+		//then join into one whole string
+	}
+	for (var key in obj){
+		if (typeof obj[key] !== 'undefined' && typeof obj[key] !== 'function'){
+			outcome.push(stringifyJSON(key)+':'+stringifyJSON(obj[key]));
+		}
+	}			
+	return '{' + outcome.join() + '}';
 };
