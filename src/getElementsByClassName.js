3f8866b7476336656ada6b54ce4673b64f0e6101
lhuynh27 <leannehuynh27@gmail.com>
2018-05-28 17:48:39 -0700

    Go through parent, and child nodes; passed test

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,16 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
-};
+var getElementsByClassName = function(className){
+	var classNameMatch = [];
+	var findClassNameMatch = function (element){
+		if (element.classList && element.classList.contains(className)){
+			classNameMatch.push(element);
+		}
+		for (var i = 0; i < element.childNodes.length; i++){
+			findClassNameMatch(element.childNodes[i]);
+		}	
+	}
+	findClassNameMatch(document.body);
+	return classNameMatch;
+}	
